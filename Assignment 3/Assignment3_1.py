880dafcc124b9f7e7a7da79e347dd52ffd6f3d1b
SatishUrewar <142568754+SatishUrewar@users.noreply.github.com>
2025-05-18 00:44:40 +0530

    Update Assignment3_1.py

--- Assignment 3/Assignment3_1.py
+++ Assignment 3/Assignment3_1.py
@@ -1,5 +1,8 @@
+#Write a program which accept N numbers from user and store it into List. Return addition of all elements from that List.
+
 from functools import reduce
 sum=lambda A,B:A+B
+
 
 def main():
     a=int(input("how many number you want to store : "))
