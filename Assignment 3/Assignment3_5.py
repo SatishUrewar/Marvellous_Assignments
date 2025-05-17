5b59c8e7e1a96cfde057c3469bba15da668f5a85
SatishUrewar <142568754+SatishUrewar@users.noreply.github.com>
2025-05-18 00:48:45 +0530

    Update Assignment3_5.py

--- Assignment 3/Assignment3_5.py
+++ Assignment 3/Assignment3_5.py
@@ -1,3 +1,4 @@
+#Write a program which accept N numbers from user and store it into List. Return addition of all prime numbers from that List. Main python file accepts N numbers from user and pass each number to ChkPrime() function which is part of our user defined module named as Marvellous Num. Name of the function from main python file should be ListPrime().
 from functools import reduce
 from MarvellousNum import ChkPrime,sum
 
